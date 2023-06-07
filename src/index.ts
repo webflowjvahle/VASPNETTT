173b70d3662bc6a477aa47e1b829e6675a40bc30
jvahle <48984317+webflowjvahle@users.noreply.github.com>
2023-06-07 08:06:28 +0200

    Updated y shift mobile

--- src/index.ts
+++ src/index.ts
@@ -25,7 +25,7 @@ const break3 = 480;
 
 function getYshift() {
   if (window.innerWidth < break3) {
-    return -0.59125;
+    return -0.79125;
   }
   if (window.innerWidth < break2) {
     return -0.5725;
